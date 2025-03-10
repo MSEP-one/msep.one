9808d876b0b94b570f7af45985b46b4997910ae4
MarianoGnu <mariano.suligoy@prehensile-tales.com>
2025-03-10 17:20:48 -0300

    Dont prevent from closing the application when there are open dialogs

---
+++ godot_project/editor/input_handlers/molecular_structures/dialog_input_handler.gd
@@ -0,0 +1,27 @@
+extends InputHandlerBase
+
+
+func get_priority() -> int:
+	return BuiltinInputHandlerPriorities.DIALOG_INPUT_HANDLER
+
+
+func handles_empty_selection() -> bool:
+	return true
+
+
+func handles_structure_context(_in_structure_context: StructureContext) -> bool:
+	return true
+
+
+func is_exclusive_input_consumer() -> bool:
+	for child: Node in Editor_Utils.get_editor().get_children():
+		if child is Window and child.visible:
+			return true
+	return false
+
+
+func forward_input(in_input_event: InputEvent, _in_camera: Camera3D, _in_context: StructureContext) -> bool:
+	if is_exclusive_input_consumer():
+		return true
+	return false
+
