14eedf21184d4b7a61ff78417839bc961ce689c3
HungryProton <hungryproton@pm.me>
2025-09-18 16:56:05 +0200

    moves 'Reference shapes' to the 'Virtual objects' ring menu

--- godot_project/editor/ring_menu_levels/virtual_objects/ring_level_virtual_objects.gd
+++ godot_project/editor/ring_menu_levels/virtual_objects/ring_level_virtual_objects.gd
@@ -22,3 +22,4 @@ func _init(in_workspace_context: WorkspaceContext, in_menu: NanoRingMenu) -> voi
 		add_action(RingActionCreateAnchorsAndSprings.new(in_workspace_context, in_menu))
 	if FeatureFlagManager.get_flag_value(FeatureFlagManager.FEATURE_FLAG_PARTICLE_EMITTERS):
 		add_action(RingActionCreateParticleEmitters.new(in_workspace_context, in_menu))
+	add_action(RingActionAddShapes.new(_workspace_context, in_menu))
