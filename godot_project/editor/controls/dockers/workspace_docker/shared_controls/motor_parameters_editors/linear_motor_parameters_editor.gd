dac547eefa3063f4cc8c140cdd1b53553754251d
HungryProton <hungryproton@pm.me>
2026-01-05 17:24:10 +0100

    adds optional force limit to linear motors

--- godot_project/editor/controls/dockers/workspace_docker/shared_controls/motor_parameters_editors/linear_motor_parameters_editor.gd
+++ godot_project/editor/controls/dockers/workspace_docker/shared_controls/motor_parameters_editors/linear_motor_parameters_editor.gd
@@ -8,6 +8,8 @@ var _parameters_wref: WeakRef = weakref(null) # WeakRef<NanoLinearMotorParameter
 var _linear_ramp_in_time: TimeSpanPicker
 var _linear_ramp_out_time: TimeSpanPicker
 var _linear_top_speed_spin_box: SpinBoxSlider
+var _limit_force_check_button: CheckButton
+var _linear_max_force_spin_box: SpinBoxSlider
 var _linear_forward_polarity_button: Button
 var _linear_backward_polarity_button: Button
 
@@ -25,12 +27,16 @@ func _notification(what: int) -> void:
 		_linear_ramp_in_time = %LinearRampInTime as TimeSpanPicker
 		_linear_ramp_out_time = %LinearRampOutTime as TimeSpanPicker
 		_linear_top_speed_spin_box = %LinearTopSpeedSpinBox as SpinBoxSlider
+		_limit_force_check_button = %LimitForceCheckButton as CheckButton
+		_linear_max_force_spin_box = %LinearMaxForceSpinBox as SpinBoxSlider
 		_linear_forward_polarity_button = %LinearForwardPolarityButton as Button
 		_linear_backward_polarity_button = %LinearBackwardPolarityButton as Button
 		# Linear controls signals
 		_linear_ramp_in_time.time_span_changed.connect(_on_linear_ramp_in_time_time_span_changed)
 		_linear_ramp_out_time.time_span_changed.connect(_on_linear_ramp_out_time_time_span_changed)
 		_linear_top_speed_spin_box.value_confirmed.connect(_on_linear_top_speed_spin_box_value_confirmed)
+		_limit_force_check_button.toggled.connect(_on_limit_force_check_button_toggled)
+		_linear_max_force_spin_box.value_confirmed.connect(_on_linear_max_force_spin_box_value_confirmed)
 		_linear_forward_polarity_button.toggled.connect(_on_linear_forward_polarity_button_toggled)
 		_linear_backward_polarity_button.toggled.connect(_on_linear_backward_polarity_button_toggled)
 
@@ -76,8 +82,11 @@ func _on_linear_motor_parameters_changed() -> void:
 	_linear_ramp_out_time.editable = parameters.cycle_type != NanoVirtualMotorParameters.CycleType.CONTINUOUS
 	var top_speed_in_femtoseconds: float = parameters.top_speed_in_nanometers_by_nanoseconds / 1e+6
 	_linear_top_speed_spin_box.set_value_no_signal(top_speed_in_femtoseconds)
+	_linear_max_force_spin_box.set_value_no_signal(parameters.max_force)
 	_linear_forward_polarity_button.set_pressed_no_signal(parameters.polarity == NanoLinearMotorParameters.Polarity.FORWARD)
 	_linear_backward_polarity_button.set_pressed_no_signal(parameters.polarity == NanoLinearMotorParameters.Polarity.BACKWARDS)
+	_limit_force_check_button.set_pressed_no_signal(parameters.limit_maximum_force)
+	_linear_max_force_spin_box.visible = parameters.limit_maximum_force
 
 
 func _on_linear_ramp_in_time_time_span_changed(in_magnitude: float, in_unit: TimeSpanPicker.Unit) -> void:
@@ -88,6 +97,7 @@ func _on_linear_ramp_in_time_time_span_changed(in_magnitude: float, in_unit: Tim
 	_get_linear_parameters().ramp_in_time_in_nanoseconds = in_magnitude
 	_take_snapshot_if_configured(tr(&"Ramp-In Time"))
 
+
 func _on_linear_ramp_out_time_time_span_changed(in_magnitude: float, in_unit: TimeSpanPicker.Unit) -> void:
 	if in_unit != TimeSpanPicker.Unit.NANOSECOND:
 		# Convert magnitude to nanoseconds
@@ -96,15 +106,29 @@ func _on_linear_ramp_out_time_time_span_changed(in_magnitude: float, in_unit: Ti
 	_get_linear_parameters().ramp_out_time_in_nanoseconds = in_magnitude
 	_take_snapshot_if_configured(tr(&"Ramp-Out Time"))
 
+
 func _on_linear_top_speed_spin_box_value_confirmed(in_top_speed_in_femtoseconds: float) -> void:
 	var top_speed_in_nanoseconds: float = in_top_speed_in_femtoseconds * 1e+6
 	_get_linear_parameters().top_speed_in_nanometers_by_nanoseconds = top_speed_in_nanoseconds
 	_take_snapshot_if_configured(tr(&"Top Speed"))
 
+
+func _on_limit_force_check_button_toggled(in_toggled: bool) -> void:
+	_get_linear_parameters().limit_maximum_force = in_toggled
+	_take_snapshot_if_configured(tr(&"Limit Maximum Force"))
+	_linear_max_force_spin_box.visible = in_toggled
+
+
+func _on_linear_max_force_spin_box_value_confirmed(in_max_force: float) -> void:
+	_get_linear_parameters().max_force = in_max_force
+	_take_snapshot_if_configured(tr(&"Maximum Force"))
+
+
 func _on_linear_forward_polarity_button_toggled(in_button_pressed: bool) -> void:
 	if in_button_pressed:
 		_get_linear_parameters().polarity = NanoLinearMotorParameters.Polarity.FORWARD
 		_take_snapshot_if_configured(tr(&"Use Forward Polarity"))
+
 
 func _on_linear_backward_polarity_button_toggled(in_button_pressed: bool) -> void:
 	if in_button_pressed:
