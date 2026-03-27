aa07c96f72743e16d4f22c999640d641f56643f7
MarianoGnu <mariano.suligoy@prehensile-tales.com>
2026-03-27 17:07:32 -0300

    Prevent moving molecules to another group when connected by atom-to-atom springs linked to an unselected molecule

--- godot_project/project_workspace/workspace_context/structure_context/selection_db/selection_db.gd
+++ godot_project/project_workspace/workspace_context/structure_context/selection_db/selection_db.gd
@@ -195,10 +195,10 @@ func select_by_type(types_to_select: PackedInt32Array) -> void:
 	_process_atom_selection_result(result)
 
 
-func select_connected(in_show_hidden_objects: bool) -> AtomSelection.AtomSelectionResult:
+func select_connected(in_show_hidden_objects: bool, in_linked_by_springs: bool) -> AtomSelection.AtomSelectionResult:
 	var nano_structure: NanoStructure = _structure_context.nano_structure
 	assert(!nano_structure.is_being_edited(), "Setting the selection while structure is changing is insecure and should be avoided")
-	var result: AtomSelection.AtomSelectionResult = _atom_selection.select_connected(in_show_hidden_objects)
+	var result: AtomSelection.AtomSelectionResult = _atom_selection.select_connected(in_show_hidden_objects, in_linked_by_springs)
 	_process_atom_selection_result(result)
 	return result
 
