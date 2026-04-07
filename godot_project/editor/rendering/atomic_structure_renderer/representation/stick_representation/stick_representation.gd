11a5318e3830c30af2b40eac61d8b7ab04d161c3
MarianoGnu <mariano.suligoy@prehensile-tales.com>
2026-04-07 12:54:17 -0300

    Don't render bonds of length zero

--- godot_project/editor/rendering/atomic_structure_renderer/representation/stick_representation/stick_representation.gd
+++ godot_project/editor/rendering/atomic_structure_renderer/representation/stick_representation/stick_representation.gd
@@ -713,6 +713,8 @@ func _calculate_partial_selection_translation(in_bond: Vector3i, in_delta_transl
 
 static func calculate_transform_for_bond(in_first_pos: Vector3, in_sec_pos: Vector3,
 			in_up_vector: Vector3) -> Transform3D:
+	if in_first_pos == in_sec_pos:
+		return Transform3D().scaled(Vector3.ZERO)
 	var particle_position: Vector3 = (in_first_pos + in_sec_pos) / 2.0
 	# Zero length bonds cause rendering issues. A length 0.025 makes the
 	# capsule stick looks like a sphere when two atoms are at the same place.
