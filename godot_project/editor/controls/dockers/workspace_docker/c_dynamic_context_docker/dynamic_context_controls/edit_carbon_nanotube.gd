4f8d390bbe34a6d2e45ad53105140e68ea402b96
MarianoGnu <mariano.suligoy@prehensile-tales.com>
2026-07-09 18:27:16 -0300

    Add an option to convert Carbon Nanotube object into a regular/simulatable atoms and bonds group

--- godot_project/editor/controls/dockers/workspace_docker/c_dynamic_context_docker/dynamic_context_controls/edit_carbon_nanotube.gd
+++ godot_project/editor/controls/dockers/workspace_docker/c_dynamic_context_docker/dynamic_context_controls/edit_carbon_nanotube.gd
@@ -6,6 +6,8 @@ var _m_spin_box_slider: SpinBoxSlider
 var _graphene_latice_preview: GrapheneLatticePreview
 var _diameter_label: Label
 var _circumference_label: Label
+var _convert_to_atoms_button: Button
+var _change_representation_button: RichTextLabel
 
 
 var _workspace_context: WorkspaceContext
@@ -36,11 +38,15 @@ func _notification(what: int) -> void:
 		_graphene_latice_preview = %GrapheneLaticePreview as GrapheneLatticePreview
 		_diameter_label = %DiameterLabel as Label
 		_circumference_label = %CircumferenceLabel as Label
+		_convert_to_atoms_button = %ConvertToAtomsButton as Button
+		_change_representation_button = %ChangeRepresentationButton as RichTextLabel
 		
 		_n_spin_box_slider.value_changed.connect(_on_n_spin_box_slider_value_changed)
 		_m_spin_box_slider.value_changed.connect(_on_m_spin_box_slider_value_changed)
 		_n_spin_box_slider.value_confirmed.connect(_on_n_spin_box_slider_value_confirmed)
 		_m_spin_box_slider.value_confirmed.connect(_on_m_spin_box_slider_value_confirmed)
+		_convert_to_atoms_button.pressed.connect(_on_convert_to_atoms_button_pressed)
+		_change_representation_button.meta_clicked.connect(_on_change_representation_button_meta_clicked)
 
 
 func _set_selected_context(out_nanotube_or_null: CarbonNanotubeStructure) -> void:
@@ -79,6 +85,43 @@ func _on_m_spin_box_slider_value_confirmed(in_value: int) -> void:
 		_edited_nanotube.set_chiral_index_m(in_value)
 		_edited_nanotube.end_edit()
 		_workspace_context.snapshot_moment("Set: Nanotube Chiral Index")
+
+
+func _on_convert_to_atoms_button_pressed() -> void:
+	assert(_edited_nanotube != null, "Invalid ui state")
+	# We need to make atoms available, but no bother other parts of the editor
+	_edited_nanotube.set_block_signals(true)
+	_edited_nanotube.set_force_track_atoms(true)
+	var parent_group: NanoStructure = _workspace_context.workspace.get_structure_by_int_guid(_edited_nanotube.int_parent_guid)
+	var new_group: AtomicStructure = AtomicStructure.create()
+	new_group.set_structure_name(_edited_nanotube.get_structure_name() + "'s atoms")
+	_workspace_context.workspace.add_structure(new_group, parent_group)
+	new_group.start_edit()
+	var atom_map: Dictionary[int, int]
+	for atom_id: int in _edited_nanotube.get_valid_atoms():
+		var atomic_number: int = _edited_nanotube.atom_get_atomic_number(atom_id)
+		var pos: Vector3 = _edited_nanotube.atom_get_position(atom_id)
+		atom_map[atom_id] = new_group.add_atom(AtomicStructure.AddAtomParameters.new(atomic_number, pos))
+	for bond_id: int in _edited_nanotube.get_bonds_ids():
+		var bond_data: Vector3i = _edited_nanotube.get_bond(bond_id)
+		# remap atom ids
+		new_group.add_bond(atom_map[bond_data.x], atom_map[bond_data.y], bond_data.z)
+	new_group.end_edit()
+	# Done polling atoms, back to normal
+	_edited_nanotube.set_force_track_atoms(false)
+	_edited_nanotube.set_block_signals(false)
+	_workspace_context.workspace.remove_structure(_edited_nanotube)
+	var new_context: StructureContext = _workspace_context.get_structure_context(new_group.int_guid)
+	new_context.select_all()
+	_workspace_context.snapshot_moment("Convert Carbon Nanotube to Atoms and Bonds")
+
+
+func _on_change_representation_button_meta_clicked(_meta: Variant) -> void:
+	MolecularEditorContext.request_workspace_docker_focus(
+		WorkspaceSettingsDocker.UNIQUE_DOCKER_NAME,
+		&"Representation Settings",
+		[^"VisibilitySettings", ^"%NanotubeRepresentationOptionButton/.."]
+		)
 
 
 func _update_estimates() -> void:
