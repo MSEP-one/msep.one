d85b09dfe84a7002170709639f1469312d7e3d02
MarianoGnu <mariano.suligoy@prehensile-tales.com>
2026-07-23 13:23:57 -0300

    Rearrange View TopBar/RingMenu - RingMenu: Toggle Dna/Nanotube representation was moved to the Representation submenu - TopBar: Toggle Dna/Nanotube was moved inside the Representation Section - TopBar: Balls and Sticks was moved to the top of the list, to match the order in the ring menu

--- godot_project/editor/ring_menu_levels/view_menu/representation_menu/ring_level_representation.gd
+++ godot_project/editor/ring_menu_levels/view_menu/representation_menu/ring_level_representation.gd
@@ -20,5 +20,7 @@ func _init(in_workspace_context: WorkspaceContext, in_menu: NanoRingMenu) -> voi
 	add_action(preload("ring_action_enhanced_sticks.gd").new(in_workspace_context, in_menu))
 	add_action(preload("ring_action_enhanced_sticks_and_balls.gd").new(in_workspace_context, in_menu))
 	add_action(preload("ring_action_atom_size_settings.gd").new(in_workspace_context, in_menu))
+	add_action(preload("ring_action_toggle_dna_representation.gd").new(in_workspace_context, in_menu))
+	add_action(preload("ring_action_toggle_nanotube_representation.gd").new(in_workspace_context, in_menu))
 	add_action(preload("ring_action_theme.gd").new(in_workspace_context, in_menu))
 	add_action(preload("ring_action_camera_projection.gd").new(in_workspace_context, in_menu))
